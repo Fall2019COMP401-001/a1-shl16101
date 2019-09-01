71f91b1db1c34b1849a87c641e11f056025c7310
Lu <Lu@LAPTOP-9MN8EO0B.wireless-1x.unc.edu>
2019-09-01 19:17:19 -0400

    First version of hw

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,9 +6,50 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		Scanner sc = new Scanner(System.in);
+		
+		int NumOfCustomer = sc.nextInt(); // 3
+		
+		String [] Result = new String [NumOfCustomer];
+		
+		for(int j=0; j < NumOfCustomer; j++) {
+			char FirstName = sc.next().charAt(0); // C
+			
+			String LastName = sc.next(); // LastName
+			
+			int count = sc.nextInt(); // 3
+			
+			// store the number in an array
+			int [] NumberOfPurchase = new int[count];
+			double [] Price = new double[count];
+			
+			double SumOfC1 = 0;
+			double SumOfSingleItem = 0;
+			// define the value of variable sum
+			
+			
+			//System.out.println(NumberOfPurchase[1]);
+		
+			
+			for (int i=0; i< count; i++) {
+				NumberOfPurchase[i] = sc.nextInt();
+				String Fruit = sc.next();
+				Price[i] = sc.nextDouble();
+				SumOfSingleItem = NumberOfPurchase[i] * Price[i];
+				SumOfC1 += SumOfSingleItem;
+			}
+			
+			Result[j] = FirstName + "." + LastName + ": " + SumOfC1;
+			// read the name of customer and printed it with F. Last format
+			
+		}
+		
+		for(int k = 0; k < NumOfCustomer; k++) {
+			System.out.println(Result[k]);
+		}
+		
+		
+		
 		
 	}
 }
